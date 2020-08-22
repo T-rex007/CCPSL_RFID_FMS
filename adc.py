b7bec88eba51c7ddcdb1aff0335ddb9f679a4587
T-rex007 <shaqc777@yahoo.com>
2020-08-22 17:26:30 -0400

    ----

--- adc.py
+++ adc.py
@@ -5,21 +5,38 @@ import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
-CLK  = 18
-MISO = 23
-MOSI = 24
-CS   = 12
-# create the spi bus
-spi = busio.SPI(clock=CLK, MISO=MISO, MOSI=MOSI)
- 
-# create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D5)
- 
-# create the mcp object
-mcp = MCP.MCP3008(spi, cs)
- 
-# create an analog input channel on pin 0
-chan = AnalogIn(mcp, MCP.P0)
- 
-print('Raw ADC Value: ', chan.value)
-print('ADC Voltage: ' + str(chan.voltage) + 'V')
+
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+
+def readADCValue(): 
+    # create the spi bus
+    spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+    
+    # create the cs (chip select)
+    cs = digitalio.DigitalInOut(board.D5)
+    
+    # create the mcp object
+    mcp = MCP.MCP3008(spi, cs)
+    
+    # create an analog input channel on pin 0
+    chan = AnalogIn(mcp, MCP.P0)
+    
+    print('Raw ADC Value: ', chan.value)
+    print('ADC Voltage: ' + str(chan.voltage) + 'V')
+    GPIO.cleanip()
+
+def readRFID():
+    reader = SimpleMFRC522()
+    try:
+            id, text = reader.read()
+            print(id)
+            print(text)
+    finally:
+            GPIO.cleanup()
+    
+if __name__=="__main__":
+    readADCValue()
+    readRFID()
